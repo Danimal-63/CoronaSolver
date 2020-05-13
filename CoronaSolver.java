73820d0b357538d995704e5d0d3fb998a1b650c8
Danimal-63 <daniel_guerrero_73@yahoo.com>
2020-05-13 13:15:03 -0700

    Update CoronaSolver.java
    Broke up the file into different parts for easy manipulation

--- CoronaSolver.java
+++ CoronaSolver.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 import java.util.*;
-
+private int n=0;
+private int f=0;
+private ArrayList newYorkList;
+private ArrayList losAngelesList;
 public class CoronaSolver {
 	Scanner scan = new Scanner(InputFileName);
 	ArrayList <Integer> list = new ArrayList <Integer>();
@@ -8,6 +11,16 @@ public class CoronaSolver {
 		while(scan.hasNext()) {
 			list.add(Integer.parseInt(scan.next()));
 		}
+		for (int i=0;i<list.size;i++){
+			if (i==0){
+				n=list.get(0);
+			}else if (i==1){
+				f=list.get(1);
+			}else if (i%2==0)
+				newYorkList.add(list.get(i));
+		}else
+			losAngelesList.add(list.get(i));
+	}
 	}
 	private int solveHelper (int num, int day, boolean flight) {
 		if(flight) {
