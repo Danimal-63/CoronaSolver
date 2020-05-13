b45ee569b7029dd357cf13c7ed34a5064bedc22a
Danimal-63 <daniel_guerrero_73@yahoo.com>
2020-05-13 13:35:05 -0700

    added comments

--- CoronaSolver.java
+++ CoronaSolver.java
@@ -4,6 +4,11 @@ private int n=0;
 private int f=0;
 private ArrayList newYorkList;
 private ArrayList losAngelesList;
+//Reads in the file InputFileName and divides it into the following parts:
+//n is the number of days the test will be ran for
+// f is the number of infections on flights
+// newYorkList holds the infection numbers for new york
+// losAngelesList holds the infection numbers for los angeles
 public class CoronaSolver {
 	Scanner scan = new Scanner(InputFileName);
 	ArrayList <Integer> list = new ArrayList <Integer>();
